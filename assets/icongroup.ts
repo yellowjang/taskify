c3f2105d6c458a2877684bd5fd5f4c8ad6d9076d
Ayoung <ay0109001@gmail.com>
2024-07-29 21:10:37 +0900

    chore: svg 파일 추가

---
+++ assets/icongroup.ts
@@ -0,0 +1,47 @@
+import IconAddBox from './icons/ic_add_box.svg';
+import IconArrowDown from './icons/ic_arrow_down.svg';
+import IconArrowForward from './icons/ic_arrow_forward.svg';
+import IconCalender from './icons/ic_calender.svg';
+import IconClose from './icons/ic_close.svg';
+import IconCodeit20 from './icons/ic_codeit_20.svg';
+import IconCodeit24 from './icons/ic_codeit_24.svg';
+import IconCodeit34 from './icons/ic_codeit_34.svg';
+import IconCodeit40 from './icons/ic_codeit_40.svg';
+import IconCrown from './icons/ic_crown.svg';
+import IconEmptyInvitation from './icons/ic_empty_invitation.svg';
+import IconVisible from './icons/ic_visible.svg';
+import IconSetting from './icons/ic_setting.svg';
+import IconSearch from './icons/ic_search.svg';
+import IconLogoSmall from './icons/ic_logo_small.svg';
+import IconLogoLarge from './icons/ic_logo_large.svg';
+import IconLogoHeader from './icons/ic_logo_header.svg';
+import IconKebab from './icons/ic_kebab.svg';
+import IconInvisible from './icons/ic_invisible.svg';
+import IconInstagram from './icons/ic_instagram.svg';
+import IconFacebook from './icons/ic_facebook.svg';
+import IconEnvelop from './icons/ic_envelop.svg';
+
+export {
+  IconAddBox,
+  IconArrowDown,
+  IconArrowForward,
+  IconCalender,
+  IconClose,
+  IconCodeit20,
+  IconCodeit24,
+  IconCodeit34,
+  IconCodeit40,
+  IconCrown,
+  IconEmptyInvitation,
+  IconVisible,
+  IconSetting,
+  IconSearch,
+  IconLogoSmall,
+  IconLogoLarge,
+  IconLogoHeader,
+  IconKebab,
+  IconInstagram,
+  IconInvisible,
+  IconFacebook,
+  IconEnvelop,
+};
