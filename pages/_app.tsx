2233d65a9c3e50c85f2acbf63f7183e232604225
mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>
2024-08-02 15:07:06 +0900

    fix: app 컴포넌트 수정 (#18)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
 import '@/styles/reset.scss';
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
