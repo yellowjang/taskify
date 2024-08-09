23a80ef6789928d19a7328d858f541a9b0b61be5
mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>
2024-08-09 13:51:37 +0900

    fix: 현재 시간으로 나오던 에러 수정 (#54)

--- utils/getDate.ts
+++ utils/getDate.ts
@@ -2,13 +2,12 @@
 // null일 경우 미정 반환
 
 function getDate(dateString: string | null, time?: boolean): string {
-  const now = new Date();
-  const utc = now.getTime() + now.getTimezoneOffset() * 60 * 1000;
-  const koreaTimeDiff = 9 * 60 * 60 * 1000;
-  const korNow = new Date(utc + koreaTimeDiff); // 한국 시간 구하기
-
   let formattedDate;
   if (typeof dateString == 'string') {
+    const now = new Date(dateString);
+    const utc = now.getTime() + now.getTimezoneOffset() * 60 * 1000;
+    const koreaTimeDiff = 9 * 60 * 60 * 1000;
+    const korNow = new Date(utc + koreaTimeDiff); // 한국 시간 구하기
     const year = korNow.getFullYear();
     const month = String(korNow.getMonth() + 1).padStart(2, '0');
     const day = String(korNow.getDate()).padStart(2, '0');
