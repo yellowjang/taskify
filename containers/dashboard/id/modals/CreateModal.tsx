c09cc28765dd298faa3e9decb307f54f9796c49b
mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>
2024-08-11 01:14:47 +0900

    refactor: create modal 에러메시지 토스트로 띄우기 (#75)

--- containers/dashboard/id/modals/CreateModal.tsx
+++ containers/dashboard/id/modals/CreateModal.tsx
@@ -3,19 +3,44 @@ import ModalPortal from '@/components/ModalPortal';
 import { useCreateModalStore } from '@/stores/modalStore';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from '@/services/axios';
+import useColumnList from '@/hooks/useColumnList';
+import useToast from '@/hooks/useToast';
+
+// 커스텀 에러만 드게 하려고 만든 클래스 (gpt가 도와줬어요..)
+class DuplicateColumnError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'DuplicateColumnError';
+  }
+}
 
 // 현재 대시보드 id를 받아옴
 function CreateModal(id: any) {
   const { isModalOpen, setCloseModal } = useCreateModalStore();
   const queryClient = useQueryClient();
+  const { toast } = useToast();
+
+  const { columnList } = useColumnList(id.id);
 
   // 컬럼 생성하는 mutate 함수
   const createColumnMutation = useMutation({
-    mutationFn: (newTitle: string) =>
-      axios.post(`/columns`, { title: newTitle, dashboardId: Number(id.id) }),
+    mutationFn: (newTitle: string) => {
+      if (columnList.some((column: IColumn) => column.title === newTitle)) {
+        throw new DuplicateColumnError('이미 존재하는 컬럼 제목입니다.');
+      }
+      return axios.post(`/columns`, {
+        title: newTitle,
+        dashboardId: Number(id.id),
+      });
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['getColumnList', id.id] });
       setCloseModal();
+    },
+    onError: (e: any) => {
+      if (e instanceof DuplicateColumnError) {
+        toast('error', e.message);
+      }
     },
   });
 
@@ -24,7 +49,11 @@ function CreateModal(id: any) {
   };
 
   const handleCreateBtnClick = (data: { title: string }) => {
-    createColumnMutation.mutate(data.title);
+    if (columnList.some((column: IColumn) => column.title === data.title)) {
+      toast('error', '이미 존재하는 컬럼 제목입니다.');
+    } else {
+      createColumnMutation.mutate(data.title);
+    }
   };
 
   if (!isModalOpen) return null;
