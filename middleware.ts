f05dd50ffe0c999948d530dd351526830eb62c0b
mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>
2024-08-12 13:13:56 +0900

    docs: 주석 제거

--- middleware.ts
+++ middleware.ts
@@ -8,29 +8,24 @@ export function middleware(request: NextRequest) {
 
   const { pathname } = request.nextUrl;
 
-  console.log('token');
-  console.log(token);
-
-  // // 로그인이 되어있지 않고 보호된 페이지에 접근하려는 경우
-  // if (!token) {
-  //   if (
-  //     pathname.startsWith('/dashboard') ||
-  //     pathname.startsWith('/mydashboard') ||
-  //     pathname.startsWith('/mypage')
-  //   ) {
-  //     return NextResponse.redirect(new URL('/', request.url));
-  //   }
-  // } else {
-  //   // 로그인이 되어있는데, 로그인 페이지나 메인 페이지에 접근하려는 경우
-  //   if (pathname === '/signin' || pathname === '/signup' || pathname === '/') {
-  //     return NextResponse.redirect(new URL('/mydashboard', request.url));
-  //   }
-  // }
+  // 로그인이 되어있지 않고 보호된 페이지에 접근하려는 경우
+  if (!token) {
+    if (
+      pathname.startsWith('/dashboard') ||
+      pathname.startsWith('/mydashboard') ||
+      pathname.startsWith('/mypage')
+    ) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  } else {
+    // 로그인이 되어있는데, 로그인 페이지나 메인 페이지에 접근하려는 경우
+    if (pathname === '/signin' || pathname === '/signup' || pathname === '/') {
+      return NextResponse.redirect(new URL('/mydashboard', request.url));
+    }
+  }
 
   // 기본적으로 요청을 그대로 반환
   return NextResponse.next();
-
-  //   return NextResponse.redirect(new URL('/about-2', request.url));
 }
 
 // See "Matching Paths" below to learn more
