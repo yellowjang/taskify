6403c5de3f0a9fa6bec6afc94192f89475770a9e
hbs0133 <126546310+hbs0133@users.noreply.github.com>
2024-08-14 01:00:13 +0900

    feat: OG태그 추가 (#146)
    * feat: OG태그 추가
    
    * fix: OG태그 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,14 @@ export default function Document() {
   return (
     <Html lang='ko'>
       <Head>
+        <title>SCHEDO</title>
+        <meta property='og:title' content='SCHEDO' />
+        <meta
+          property='og:description'
+          content='SCHEDO에서 모든 일정을 쉽게 관리하세요!'
+        />
+        <meta property='og:url' content='https://sched6.netlify.app/' />
+        <meta property='og:image' content='/og_image.png' />
         <link rel='icon' href='/favicon.png' />
       </Head>
       <body>
